52fad30e283875efe504380afc3da6ebc583d5cc
k2-nakayama <k2-nakayama@nri.co.jp>
2021-03-21 10:09:05 +0900

    鍵交換部分まで検証

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,12 @@
 import org.bouncycastle.crypto.engines.DESEngine
 import org.bouncycastle.crypto.macs.ISO9797Alg3Mac
 import org.bouncycastle.crypto.params.KeyParameter
-import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.MessageDigest
 import javax.crypto.Cipher
 import javax.crypto.SecretKeyFactory
-import javax.crypto.spec.DESKeySpec
 import javax.crypto.spec.DESedeKeySpec
 import javax.crypto.spec.IvParameterSpec
-import javax.crypto.Mac
-
-import java.security.Security
+import kotlin.experimental.xor
 
 
 fun main() {
@@ -40,7 +36,7 @@ fun main() {
     val encData = rndIfd + rndIcc + keyIfd
     printByteArray("encData", encData)
 
-    val encIfd = tdes2key(
+    val encIfd = tdes2keyEncode(
         keyEnc,
         encData
     )
@@ -75,15 +71,112 @@ fun main() {
     )
     println("-----------------------------------------------------------------------------")
 
+    val encIcc = byteArrayOf(
+        0x58.toByte(), 0x60.toByte(), 0x77.toByte(), 0x5B.toByte(),
+        0x4D.toByte(), 0x03.toByte(), 0x2C.toByte(), 0xC5.toByte(),
+        0x64.toByte(), 0xBA.toByte(), 0x20.toByte(), 0x4B.toByte(),
+        0x8E.toByte(), 0xA8.toByte(), 0x68.toByte(), 0xF6.toByte(),
+        0x94.toByte(), 0xA7.toByte(), 0x4E.toByte(), 0x74.toByte(),
+        0x75.toByte(), 0xA8.toByte(), 0xFE.toByte(), 0xF2.toByte(),
+        0x40.toByte(), 0x58.toByte(), 0x8B.toByte(), 0xDA.toByte(),
+        0x1A.toByte(), 0xF4.toByte(), 0x96.toByte(), 0xCE.toByte()
+    )
+    printByteArray("encIcc", encIcc)
+
+    println("-----------------------------------------------------------------------------")
+    val macIcc = byteArrayOf(
+        0x59.toByte(), 0x38.toByte(), 0x8F.toByte(), 0xD6.toByte(),
+        0xCD.toByte(), 0x45.toByte(), 0x24.toByte(), 0x8B.toByte()
+    )
+    printByteArray("macIcc", macIcc)
+    printByteArray(
+        "expect",
+        retailMac(
+            keyMac,
+            encIcc
+        )
+    )
+    println("-----------------------------------------------------------------------------")
+
+    val decIcc = tdes2keyDecode(
+        keyEnc,
+        encIcc
+    )
+    printByteArray("decIcc", decIcc)
+
+    println("-----------------------------------------------------------------------------")
+    printByteArray("rndIcc", rndIcc)
+    printByteArray("expect", decIcc.copyOfRange(0, 8))
+    println("-----------------------------------------------------------------------------")
+    printByteArray("rndIfd", rndIfd)
+    printByteArray("expect", decIcc.copyOfRange(8, 16))
+    println("-----------------------------------------------------------------------------")
+
+    val keyIcc = decIcc.copyOfRange(16, 32)
+    printByteArray("keyIcc", keyIcc)
+    printByteArray(
+        "expect",
+        byteArrayOf(
+            0x19.toByte(), 0xD0.toByte(), 0x49.toByte(), 0x49.toByte(),
+            0x0F.toByte(), 0xFF.toByte(), 0x52.toByte(), 0xEE.toByte(),
+            0xDB.toByte(), 0xFC.toByte(), 0xB9.toByte(), 0x30.toByte(),
+            0xBC.toByte(), 0x81.toByte(), 0x0E.toByte(), 0xD0.toByte()
+        )
+    )
+    println("-----------------------------------------------------------------------------")
+
+    val xorKey = xorByteArray(keyIfd, keyIcc)
+    printByteArray("xorKey", xorKey)
+    printByteArray(
+        "expect",
+        byteArrayOf(
+            0x59.toByte(), 0x91.toByte(), 0x0B.toByte(), 0x0A.toByte(),
+            0x4B.toByte(), 0xBA.toByte(), 0x14.toByte(), 0xA9.toByte(),
+            0x93.toByte(), 0xB5.toByte(), 0xF3.toByte(), 0x7B.toByte(),
+            0xF0.toByte(), 0xCC.toByte(), 0x40.toByte(), 0x9F.toByte()
+        )
+    )
+    println("-----------------------------------------------------------------------------")
+
+    val keySessionEnc = MessageDigest.getInstance("SHA-1").digest(
+        xorKey + byteArrayOf(0x00, 0x00, 0x00, 0x01)
+    ).copyOfRange(0, 16)
+    printByteArray("keySessionEnc", keySessionEnc)
+    printByteArray(
+        "expectKeySEnc",
+        byteArrayOf(
+            0xCE.toByte(), 0x94.toByte(), 0x93.toByte(), 0x8E.toByte(),
+            0x19.toByte(), 0xE3.toByte(), 0xB9.toByte(), 0x7D.toByte(),
+            0xF9.toByte(), 0x6E.toByte(), 0xAB.toByte(), 0xCE.toByte(),
+            0xDC.toByte(), 0x17.toByte(), 0x15.toByte(), 0xCC.toByte()
+        )
+    )
+
 }
 
-fun tdes2key(
+fun tdes2keyEncode(
     key: ByteArray,
     data: ByteArray
 ): ByteArray {
     val cipherKey1 = Cipher.getInstance("DESede/CBC/NoPadding").apply {
         init(
             Cipher.ENCRYPT_MODE,
+            SecretKeyFactory.getInstance("DESede").generateSecret(DESedeKeySpec(key + key.copyOfRange(0, 8))),
+            IvParameterSpec(ByteArray(8))
+        )
+    }
+    return data.let {
+        cipherKey1.doFinal(it)
+    }
+}
+
+fun tdes2keyDecode(
+    key: ByteArray,
+    data: ByteArray
+): ByteArray {
+    val cipherKey1 = Cipher.getInstance("DESede/CBC/NoPadding").apply {
+        init(
+            Cipher.DECRYPT_MODE,
             SecretKeyFactory.getInstance("DESede").generateSecret(DESedeKeySpec(key + key.copyOfRange(0, 8))),
             IvParameterSpec(ByteArray(8))
         )
@@ -111,6 +204,15 @@ fun retailMac(
     mac.doFinal(result, 0)
     return result
 
+}
+
+fun xorByteArray(
+    data1: ByteArray,
+    data2: ByteArray
+): ByteArray {
+    return data1.mapIndexed { index, byte ->
+        byte xor data2[index]
+    }.toByteArray()
 }
 
 fun printByteArray(
